e9ab71acf89dab4a8fd835c75402f93deee7513f
Helliozz <helliozz228@gmail.com>
2024-05-28 21:21:32 +0300

    first commit

---
+++ app/src/main/java/com/example/diplomafirst/WorkFragment.kt
@@ -0,0 +1,29 @@
+package com.example.diplomafirst
+
+import android.os.Bundle
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import androidx.fragment.app.Fragment
+import androidx.navigation.findNavController
+import com.example.diplomafirst.databinding.FragmentWorkBinding
+
+class WorkFragment : Fragment() {
+
+    private lateinit var binding: FragmentWorkBinding
+
+    override fun onCreateView(
+        inflater: LayoutInflater, container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View? {
+        binding = FragmentWorkBinding.inflate(layoutInflater, container, false)
+        return binding.root
+    }
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        binding.signOut.setOnClickListener {
+            requireView().findNavController()
+                .navigate(R.id.action_WorkFragment_to_SignInFragment)
+        }
+    }
+}
